cd3aec1d3b6c1781003e295766990a836c26520e
Chandler Cain <chandler@clcain.com>
2020-08-24 00:34:02 -0400

    Fix issue with quotes in openssl command

--- gen-self-signed-cert.sh
+++ gen-self-signed-cert.sh
@@ -11,4 +11,4 @@ sudo mkdir -p letsencrypt/live/$1/
 sudo openssl req -x509 -newkey rsa:4096 \
     -keyout letsencrypt/live/$1/privkey.pem \
     -out letsencrypt/live/$1/fullchain.pem \
-    -days 365 -nodes -subj '/CN=$1'
+    -days 365 -nodes -subj "/CN=$1"
